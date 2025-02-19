c1474e4f610acc94a108201ae52b746c16d0c7fb
Nico Vece <me@nicovece.com>
2025-02-19 09:39:15 +0100

    Created array of objects with name, height, types, weight of the Pokémon

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let pokemonList = [
+  { name: 'Venusaur', height: 2, types: ['grass', 'poison'], weight: 100 },
+  { name: 'Charizard', height: 1.7, types: ['fire', 'flying'], weight: 90 },
+  { name: 'Blastoise', height: 1.6, types: ['water'], weight: 85 },
+  { name: 'Butterfree', height: 1.1, types: ['bug', 'flying'], weight: 32 },
+  { name: 'Beedrill', height: 1, types: ['bug', 'poison'], weight: 29 },
+  { name: 'Pidgeot', height: 1.5, types: ['normal', 'flying'], weight: 39 },
+  { name: 'Raticate', height: 0.7, types: ['normal'], weight: 18 },
+];
