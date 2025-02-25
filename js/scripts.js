27730f8ff169fe14e4c34cef127b0b524fdc28a8
Nico Vece <me@nicovece.com>
2025-02-25 17:46:10 +0100

    used shorthand because they key and value getAll were the same

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,66 @@
-/* Array of objects with Pokémon data */
-let pokemonList = [
-  {
-    name: 'Venusaur',
-    height: 2,
-    types: ['grass', 'poison'],
-    weight: 100
-  },
-  {
-    name: 'Dragonite',
-    height: 2.2,
-    types: ['dragon', 'flying'],
-    weight: 210
-  },
-  {
-    name: 'Charizard',
-    height: 1.7,
-    types: ['fire', 'flying'],
-    weight: 90
-  },
-  {
-    name: 'Blastoise',
-    height: 1.6,
-    types: ['water'],
-    weight: 85
-  },
-  {
-    name: 'Butterfree',
-    height: 1.1,
-    types: ['bug', 'flying'],
-    weight: 32
-  },
-  {
-    name: 'Beedrill',
-    height: 1,
-    types: ['bug', 'poison'],
-    weight: 29
-  },
-  {
-    name: 'Pidgeot',
-    height: 1.5,
-    types: ['normal', 'flying'],
-    weight: 39
-  },
-  {
-    name: 'Raticate',
-    height: 0.7,
-    types: ['normal'],
-    weight: 18
+/* pokemonlist IIFE */
+let pokemonRepository = (function () {
+  /* Array of objects with Pokémon data */
+  let pokemonList = [
+    {
+      name: 'Venusaur',
+      height: 2,
+      types: ['grass', 'poison'],
+      weight: 100
+    },
+    {
+      name: 'Dragonite',
+      height: 2.2,
+      types: ['dragon', 'flying'],
+      weight: 210
+    },
+    {
+      name: 'Charizard',
+      height: 1.7,
+      types: ['fire', 'flying'],
+      weight: 90
+    },
+    {
+      name: 'Blastoise',
+      height: 1.6,
+      types: ['water'],
+      weight: 85
+    },
+    {
+      name: 'Butterfree',
+      height: 1.1,
+      types: ['bug', 'flying'],
+      weight: 32
+    },
+    {
+      name: 'Beedrill',
+      height: 1,
+      types: ['bug', 'poison'],
+      weight: 29
+    },
+    {
+      name: 'Pidgeot',
+      height: 1.5,
+      types: ['normal', 'flying'],
+      weight: 39
+    },
+    {
+      name: 'Raticate',
+      height: 0.7,
+      types: ['normal'],
+      weight: 18
+    }
+  ];
+
+  function getAll() {
+    return pokemonList;
   }
-];
+
+  return {
+    getAll
+  };
+})();
+
 /* Insert header */
 document.write(
   '<header class="page-header"><h1 class="page-header__item">PokémonĀkaibu</h1></header>'
@@ -58,7 +70,7 @@ document.write('<main><section class="content--main">');
 /* Insert pokemon list */
 document.write('<ul class="pokelist">');
 /* Loop through the array and display Pokémon data */
-pokemonList.forEach(function (pokemon) {
+pokemonRepository.getAll().forEach((pokemon) => {
   document.write(
     `<li class="pokelist__item"><h2 class="pokelist__name">${pokemon.name}</h2> <span class="pokelist__data">height: ${pokemon.height}</span>`
   );
